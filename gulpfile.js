757f6e5f96059ff7da83afbffb665751f268e290
Jon Tmarz <34428004+Jonedi@users.noreply.github.com>
2020-09-03 21:56:20 -0500

    configuración nuevo video

--- gulpfile.js
+++ gulpfile.js
@@ -2,38 +2,40 @@ const gulp = require('gulp'),
       sass = require('gulp-sass'),
       autoprefixer = require('gulp-autoprefixer'),
       minifiyCSS = require('gulp-minify-css'),
-      concat = require('gulp-concat');
+      concat = require('gulp-concat'),
+      sourcemaps = require('gulp-sourcemaps'),
       browserSync = require('browser-sync'),
       reload = browserSync.reload;
 
 gulp.task('browser-sync', ()=> {
-    const files =[
-      './sass',
-      '*/*.js',
-      '*/*.html'
-    ];
-  
-    browserSync.init(files, {
-      proxy:'http://localhost/reciclados-novo/novo/',
-      notify: false
-    });
-  })
-  
-  gulp.task('scss-task', () => {
-    return gulp.src('./sass/*.scss').
-            pipe(sass({
-              outputStyle: 'compressed',
-              sourceComments: true
-            })).
-            pipe(autoprefixer()).
-            pipe(concat('styles.css')).
-            pipe(minifiyCSS()).
-            pipe(reload({stream:true})).
-            pipe(gulp.dest('./css/'))
+  const files =[
+    './sass',
+    '/*.js',
+    '/*.html'
+  ];
+
+  browserSync.init(files, {
+    proxy:'localhost/reciclados-novo/novo/',
+    notify: false
   });
+})
   
-  gulp.task('watch', () => {
-    gulp.watch('sass/*.scss', gulp.series('scss-task'));
-  });
-  
-  gulp.task('default', gulp.parallel('scss-task', 'browser-sync', 'watch'));
+gulp.task('scss-task', () => {
+  return gulp.src('./sass/*.scss').
+        pipe(sourcemaps.init({loadMaps: true})).
+        pipe(sass({
+          outputStyle: 'compressed',
+          sourceComments: true
+        })).
+        pipe(autoprefixer()).
+        pipe(sourcemaps.write()).
+        pipe(concat('styles.css')).
+        pipe(reload({stream:true})).
+        pipe(gulp.dest('./css/'))
+});
+
+gulp.task('watch', () => {
+  gulp.watch('sass/*.scss', gulp.series('scss-task'));
+});
+
+gulp.task('default', gulp.parallel('scss-task', 'browser-sync', 'watch'));
